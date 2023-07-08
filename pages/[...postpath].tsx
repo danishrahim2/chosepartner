ccb00d089a062cf81ade33b4c210cd19536ca63d
danishrahim2 <134365380+danishrahim2@users.noreply.github.com>
2023-07-08 12:00:18 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -17,7 +17,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
     return {
       redirect: {
         permanent: false,
-        destination: `https://www.prosolutionss.com/2023/07/play-video.html` + encodeURI(path as string)
+        destination: `https://www.prosolutionss.com/2023/07/play-video.html/` + encodeURI(path as string)
 				}`,
 			},
 		};
