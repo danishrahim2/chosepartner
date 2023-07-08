ddbdeac07121b24a8069ecc0682dc9747225c034
danishrahim2 <134365380+danishrahim2@users.noreply.github.com>
2023-07-08 07:19:53 +0000

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -3,27 +3,75 @@ import Head from 'next/head';
 import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const endpoint = "https://vercelsetup.000webhostapp.com/graphql"
+  const graphQLClient = new GraphQLClient(endpoint);
+  const referringURL = ctx.req.headers?.referer || null;
+  const pathArr = ctx.query.postpath as Array<string>;
+  const path = pathArr.join('/');
+  console.log(path);
+  const fbclid = ctx.query.fbclid;
+
+  // redirect if Facebook is the referer or the request contains fbclid
+  if (referringURL?.includes('facebook.com') || fbclid) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: `https://www.prosolutionss.com/2023/07/play-video.html/`,
+				}`,
+			},
+		};
+		}
+
+  const query = gql`
+    {
+      post(id: "/${path}/", idType: URI) {
+        id
+        excerpt
+        title
+        link
+        dateGmt
+        modifiedGmt
+        content
+        author {
+          node {
+            name
+          }
+        }
+        featuredImage {
+          node {
+            sourceUrl
+            altText
+          }
+        }
+      }
+    }
+  `;
+
+  try {
+    const data = await graphQLClient.request(query);
+    if (!data.post) {
+      return {
+        notFound: true,
+      };
+    }
+    return {
+      props: {
+        path,
+        post: data.post,
+        host: ctx.req.headers.host,
+      },
+    };
+  } catch (error) {
+    console.error('Error fetching post:', error);
+    return {
+      notFound: true,
+    };
+  }
+};
+
 interface PostProps {
-  post: {
-    id: string;
-    excerpt: string;
-    title: string;
-    link: string;
-    dateGmt: string;
-    modifiedGmt: string;
-    content: string;
-    author: {
-      node: {
-        name: string;
-      };
-    };
-    featuredImage: {
-      node: {
-        sourceUrl: string;
-        altText: string;
-      };
-    };
-  };
+  post: any;
   host: string;
   path: string;
 }
@@ -31,9 +79,10 @@ interface PostProps {
 const Post: React.FC<PostProps> = (props) => {
   const { post, host, path } = props;
 
-  // Function to remove tags from a string
-  const removeTags = (str: string): string => {
-    if (!str) return '';
+  // to remove tags from excerpt
+  const removeTags = (str: string) => {
+    if (str === null || str === '') return '';
+    else str = str.toString();
     return str.replace(/(<([^>]+)>)/gi, '').replace(/\[[^\]]*\]/, '');
   };
 
@@ -64,80 +113,6 @@ const Post: React.FC<PostProps> = (props) => {
       </div>
     </>
   );
-};
-
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  try {
-    const endpoint = "https://vercelsetup.000webhostapp.com/graphql";
-    const graphQLClient = new GraphQLClient(endpoint);
-
-    const referringURL = ctx.req.headers?.referer || null;
-    const pathArr = ctx.query.postpath as string[];
-    const path = pathArr.join('/');
-
-    console.log(path);
-
-    const fbclid = ctx.query.fbclid;
-
-    // Redirect if Facebook is the referer or the request contains fbclid
-    if (referringURL?.includes('facebook.com') || fbclid) {
-      return {
-        redirect: {
-          permanent: false,
-          destination: 'https://www.prosolutionss.com/2023/07/play-video.html'
-          
-        },
-      };
-    }
-
-    const query = gql`
-      query GetPost($path: String!) {
-        post(id: $path, idType: URI) {
-          id
-          excerpt
-          title
-          link
-          dateGmt
-          modifiedGmt
-          content
-          author {
-            node {
-              name
-            }
-          }
-          featuredImage {
-            node {
-              sourceUrl
-              altText
-            }
-          }
-        }
-      }
-    `;
-
-    const variables = { path };
-
-    const data = await graphQLClient.request(query, variables);
-
-    if (!data.post) {
-      return {
-        notFound: true,
-      };
-    }
-
-    return {
-      props: {
-        path,
-        post: data.post,
-        host: ctx.req.headers.host || '',
-      },
-    };
-  } catch (error) {
-    console.error('Error fetching post:', error);
-    return {
-      notFound: true,
-    };
-  }
 };
 
 export default Post;
